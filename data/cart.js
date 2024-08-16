1c300230a09ed943b482b85a5e8551f9f42bb2d5
Monisha <priyanisharaman@gmail.com>
2024-08-16 10:25:22 +0530

    Fixed price formattig and radio selectors

--- data/cart.js
+++ data/cart.js
@@ -2,6 +2,10 @@ export const cart = [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2
+    },
+    {
+      productId : "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+      quantity : 3
     }
 
 ];
